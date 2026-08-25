2f86baa705a6ded3c204123534d473e18082e3b7
MrNeon777 <kirillov.lesha@gmail.com>
2026-08-25 22:21:57 +0200

    Added actions to buttons

--- Homework_for_Sitec_Systems/v1.1/v1_1/window.cpp
+++ Homework_for_Sitec_Systems/v1.1/v1_1/window.cpp
@@ -1,5 +1,7 @@
 #include "window.h"
 #include <QPushButton>
+#include <QMessageBox>
+#include <QDebug>
 
 Window::Window(QWidget *parent) :
     QWidget(parent)
@@ -8,12 +10,21 @@ Window::Window(QWidget *parent) :
     setFixedSize(450, 300);
 
     // Create and position buttons
-    m_button = new QPushButton("Hello World", this);
-    m_button->setGeometry(10, 10, 80, 30);
+    m_button1 = new QPushButton("Hello World", this);
+    m_button1->setGeometry(10, 10, 80, 30);
 
-    m_button = new QPushButton("Hello World", this);
-    m_button->setGeometry(110, 10, 80, 30);
+    m_button2 = new QPushButton("Hello World", this);
+    m_button2->setGeometry(110, 10, 80, 30);
 
-    m_button = new QPushButton("Hello World", this);
-    m_button->setGeometry(210, 10, 80, 30);
+    m_button3 = new QPushButton("Hello World", this);
+    m_button3->setGeometry(210, 10, 80, 30);
+
+    connect(m_button1, &QPushButton::clicked, this, &Window::button1Clicked );
+}
+
+void Window::button1Clicked() {
+    void button1Clicked();
+    QMessageBox msgBox(this);
+    msgBox.setText("The document has been modified.");
+    msgBox.exec();
 }
--- Homework_for_Sitec_Systems/v1.1/v1_1/window.h
+++ Homework_for_Sitec_Systems/v1.1/v1_1/window.h
@@ -8,8 +8,13 @@ class Window : public QWidget
 {
 public:
     explicit Window(QWidget *parent = 0);
+
 private:
-    QPushButton *m_button;
+    QPushButton *m_button1;
+    QPushButton *m_button2;
+    QPushButton *m_button3;
+public slots:
+    void button1Clicked();
 };
 
 #endif // WINDOW_H
